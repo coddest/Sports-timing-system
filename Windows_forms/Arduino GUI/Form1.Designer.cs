06ce7b302f5592255b55d66c8d386e5f6ce9fa61
Coddest <gabrielseroczynski@gmail.com>
2022-05-16 23:04:59 +0200

    stuff

--- Windows_forms/Arduino GUI/Form1.Designer.cs
+++ Windows_forms/Arduino GUI/Form1.Designer.cs
@@ -32,9 +32,9 @@
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.timeResultBox = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
             this.settings_button = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
@@ -49,7 +49,7 @@
             // 
             this.timeResultBox.Location = new System.Drawing.Point(250, 72);
             this.timeResultBox.Name = "timeResultBox";
-            this.timeResultBox.Size = new System.Drawing.Size(57, 20);
+            this.timeResultBox.Size = new System.Drawing.Size(101, 20);
             this.timeResultBox.TabIndex = 0;
             // 
             // panel1
@@ -63,6 +63,16 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(117, 336);
             this.panel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Dashboard";
             // 
             // settings_button
             // 
@@ -82,16 +92,6 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Add Run";
             this.button1.UseVisualStyleBackColor = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(3, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(111, 24);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Dashboard";
             // 
             // label2
             // 
@@ -124,7 +124,7 @@
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.timeResultBox);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Sport timing system";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
