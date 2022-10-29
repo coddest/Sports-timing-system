03b3db52806f21db0f8c996082a7d37e5b53dfa3
coddest <gabrielseroczynski@gmail.com>
2022-10-30 00:49:42 +0200

    change to potentiometer instead of a button

--- Arduino/full_LCD_version/start/start.ino
+++ Arduino/full_LCD_version/start/start.ino
@@ -5,7 +5,7 @@ SoftwareSerial HC12(9, 8); // HC-12 TX Pin, HC-12 RX Pin
 
 bool run_on = false; // is the run in progress?
 short int start_pin = 2;  // the number of the pushbutton pin
-short int input_start = 0; // toggle what input is recognized as start
+short int input_start = 768; // toggle what input is recognized as start
 
 void setup()
 {
@@ -17,7 +17,7 @@ void setup()
 
 void loop()
 {
-  if(digitalRead(start_pin)==input_start)
+  if(analogRead(A0)>=input_start)
   {
     HC12.write("run-started"); // send message over HC-12 that run has started
     Serial.println("run-started"); // for debugging
